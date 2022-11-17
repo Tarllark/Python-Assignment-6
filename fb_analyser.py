6c745c6ddb377aa741e62dd051c6720cb0a05371
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-17 11:12:28 +0000

    Adding tarfile member sanitization to extractall()

--- fb_analyser.py
+++ fb_analyser.py
@@ -24,7 +24,29 @@ def unpack(file):
 			zip_ref.extratall()
 	elif file.endswith('.tar.gz') or file.endswith('.tgz') :
 		with tarfile.open(file, 'r:gz') as tar_ref:
-			tar_ref.extractall()
+
+import os
+
+def is_within_directory(directory, target):
+	
+	abs_directory = os.path.abspath(directory)
+	abs_target = os.path.abspath(target)
+
+	prefix = os.path.commonprefix([abs_directory, abs_target])
+	
+	return prefix == abs_directory
+
+def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+	for member in tar.getmembers():
+		member_path = os.path.join(path, member.name)
+		if not is_within_directory(path, member_path):
+			raise Exception("Attempted Path Traversal in Tar File")
+
+	tar.extractall(path, members, numeric_owner=numeric_owner) 
+	
+
+safe_extract(tar_ref)
 		#tar_ref = tarfile.open
 	elif file.endswith('.txt.gz'):
 		with gzip.open(file, 'r') as tar_ref:
